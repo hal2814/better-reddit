1bf007f0472d50fd5fdd9e8effc50a44475e3a92
Dan Danilyuk and Nate McGregor <nate@>
2017-10-30 16:46:44 -0700

    start taking form

--- src/app/new/new.component.ts
+++ src/app/new/new.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../post.model';
+import { User } from '../user.model';
+import { USERS } from '../mock-users';
 
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
   styleUrls: ['./new.component.css']
 })
+
 export class NewComponent {
 
+
+submitForm(user: User, title: string, content: string, id: number) {
+    let newPostToAdd: Post = new Post(user, title, content, 0, 0, id);
+  }
 }
